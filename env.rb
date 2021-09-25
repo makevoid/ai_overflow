2f37e73b19cb0021535a4a6a48e556f72de373d5
Francesco Canessa (makevoid) <makevoid@gmail.com>
2021-09-25 12:46:16 +0100

    docker expose var

--- env.rb
+++ env.rb
@@ -4,7 +4,12 @@ Bundler.require :default
 
 PATH = File.expand_path "../", __FILE__
 
-require_relative "env_secret"
+unless ENV["DOCKER"] == "1"
+  require_relative "env_secret"
+else
+  OPENAI_TOKEN = ENV["OPENAI_TOKEN"]
+end
+
 require_relative "lib/monkeypatches"
 require_relative "lib/gpt3_davinci_codex"
 require_relative "lib/gpt3_answer_bot"
